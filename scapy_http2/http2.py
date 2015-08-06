f37848d8f51fe63df041d5efb08eb1dd5a3e97e4
Alex Moneger <alex.moneger@citrix.com>
2015-08-06 00:13:37 -0700

    All HTTP2 frames supported
    - Moved packet padding from frames to a seperate layer
    - Changes stream values to integers
    - Fixed stream fields length
    - Updated bit field calculations

--- scapy_http2/http2.py
+++ scapy_http2/http2.py
@@ -1,13 +1,13 @@
 #! -*- coding: utf-8 -*-
 
 import collections
-import os
+import random
 import struct
 
-from scapy.fields import ByteEnumField, ByteField, ConditionalField, FieldLenField, FlagsField, IntEnumField, LenField, \
-    PacketListField, StrField, StrFixedLenField, StrLenField
+from scapy.fields import ByteEnumField, ByteField, ConditionalField, FieldLenField, FlagsField, IntEnumField, IntField, \
+    LenField, PacketListField, ShortEnumField, StrField, StrFixedLenField, StrLenField
 from scapy.layers.inet import TCP
-from scapy.packet import bind_layers, Packet
+from scapy.packet import bind_layers, Packet, Padding
 
 
 class ByteLenField(LenField):
@@ -54,7 +54,7 @@ HTTP2_FRAME_TYPES = {0x00: "DATA",
 
 HTTP2FrameTypes = Dict2Enum(HTTP2_FRAME_TYPES)
 
-HTTP2_FLAGS = collections.OrderedDict(((0x01, "END_STREAM"),
+HTTP2_FLAGS = collections.OrderedDict(((0x01, "ACK"),
                                        (0x02, "UNUSED_2"),
                                        (0x04, "END_HEADERS"),
                                        (0x08, "PADDED"),
@@ -82,16 +82,27 @@ HTTP2_ERROR_CODES = {0x00: "NO_ERROR",
 
 HTTP2ErrorCodes = Dict2Enum(HTTP2_ERROR_CODES)
 
+HTTP2_SETTING_IDS = {0x01: "SETTINGS_HEADER_TABLE_SIZE",
+                     0x02: "SETTINGS_ENABLE_PUSH",
+                     0x03: "SETTINGS_MAX_CONCURRENT_STREAMS",
+                     0x04: "SETTINGS_INITIAL_WINDOW_SIZE",
+                     0x05: "SETTINGS_MAX_FRAME_SIZE",
+                     0x06: "SETTINGS_MAX_HEADER_LIST_SIZE"}
+
+HTTP2SettingIds = Dict2Enum(HTTP2_SETTING_IDS)
+
+
 def has_flag_set(pkt, flag):
     flag_index = HTTP2_FLAGS.keys().index(flag)
     return True if pkt.haslayer(HTTP2Frame) and (pkt[HTTP2Frame].flags & flag) >> flag_index == 1 else False
 
+
 class HTTP2Frame(Packet):
-    name = "HTTP2 Frame"
+    name = "HTTP2 Frame Header"
     fields_desc = [ByteLenField("length", None, width=3),
                    ByteEnumField("type", HTTP2FrameTypes.DATA, HTTP2_FRAME_TYPES),
                    FlagsField("flags", 0, 8, HTTP2_FLAGS.values()),
-                   StrFixedLenField("stream_id", "\x00" * 32, 32)]
+                   IntField("stream", 0)]
 
 
 class HTTP2PaddedFrame(Packet):
@@ -99,7 +110,8 @@ class HTTP2PaddedFrame(Packet):
     def pre_dissect(self, s):
         if self.underlayer is not None and has_flag_set(self.underlayer, HTTP2Flags.PADDED):
             padding_length = ord(s[0])
-            self.padding = s[-padding_length:]
+            self.add_payload(Padding(load=s[-padding_length:]))
+            # self.fields["padding"] = s[-padding_length:]
             s = s[:-padding_length]
         return s
 
@@ -113,34 +125,87 @@ underlayer_has_priority_flag_set = lambda pkt: underlayer_has_flag_set(pkt, HTTP
 
 
 class HTTP2Data(HTTP2PaddedFrame):
-    name = "%s Frame" % HTTP2_FRAME_TYPES[HTTP2FrameTypes.DATA]
-    fields_desc = [ConditionalField(FieldLenField("padding_length", None, length_of="padding", fmt="B"),
-                                    underlayer_has_padding_flag_set),
-                   StrField("data", ":method = GET\r\n:scheme = https\r\n:path = /\r\n"),
-                   ConditionalField(StrLenField("padding", "", length_from=lambda pkt: pkt.padding_length),
-                                    underlayer_has_padding_flag_set)]
+    name = "HTTP2 %s Frame" % HTTP2_FRAME_TYPES[HTTP2FrameTypes.DATA]
+    fields_desc = [ConditionalField(LenField("padding_length", None, fmt="B"), underlayer_has_padding_flag_set),
+                   StrField("data", ":method = GET\r\n:scheme = https\r\n:path = /\r\n") ]
 
 
 class HTTP2Headers(HTTP2PaddedFrame):
-    name = "%s Frame" % HTTP2_FRAME_TYPES[HTTP2FrameTypes.HEADERS]
-    fields_desc = [ConditionalField(FieldLenField("padding_length", None, length_of="padding", fmt="B"),
-                                    underlayer_has_padding_flag_set),
-                   ConditionalField(StrFixedLenField("dependency", "\x00" * 32, 32), underlayer_has_priority_flag_set),
+    name = "HTTP2 %s Frame" % HTTP2_FRAME_TYPES[HTTP2FrameTypes.HEADERS]
+    fields_desc = [ConditionalField(LenField("padding_length", None, fmt="B"), underlayer_has_padding_flag_set),
+                   ConditionalField(IntField("dependency_stream", 0), underlayer_has_priority_flag_set),
                    ConditionalField(ByteField("weight", 0), underlayer_has_priority_flag_set),
                    # Encoding for => Host: 127.0.0.1
-                   StrField("headers", "f\x87\x08\x9d\\\x0b\x81p\xff"),
-                   ConditionalField(StrLenField("padding", "", length_from=lambda pkt: pkt.padding_length),
-                                    underlayer_has_padding_flag_set)]
+                   StrField("headers", "f\x87\x08\x9d\\\x0b\x81p\xff") ]
 
 
 class HTTP2Priority(Packet):
-    name = "%s Frame" % HTTP2_FRAME_TYPES[HTTP2FrameTypes.PRIORITY]
-    fields_desc = [StrFixedLenField("dependency", "\x00" * 32, 32),
+    name = "HTTP2 %s Frame" % HTTP2_FRAME_TYPES[HTTP2FrameTypes.PRIORITY]
+    fields_desc = [IntField("dependency_stream", 0),
                    ByteField("weight", 0)]
 
+
 class HTTP2RstStream(Packet):
-    name = "%s Frame" % HTTP2_FRAME_TYPES[HTTP2FrameTypes.RST_STREAM]
+    name = "HTTP2 %s Frame" % HTTP2_FRAME_TYPES[HTTP2FrameTypes.RST_STREAM]
     fields_desc = [IntEnumField("error_code", HTTP2ErrorCodes.NO_ERROR, HTTP2_ERROR_CODES)]
+
+
+class HTTP2Setting(Packet):
+    name = "HTTP2 Setting"
+    fields_desc = [ShortEnumField("id", HTTP2SettingIds.SETTINGS_HEADER_TABLE_SIZE, HTTP2_SETTING_IDS),
+                   IntField("value", 4096)]
+
+
+class HTTP2Settings(Packet):
+    name = "HTTP2 %s Frame" % HTTP2_FRAME_TYPES[HTTP2FrameTypes.SETTINGS]
+    fields_desc = [PacketListField("settings", None, HTTP2Setting)]
+
+    def do_dissect(self, s):
+        pos = 0
+        setting_len = len(HTTP2Setting())
+        settings = []
+        while pos < len(s):
+            if len(s[pos: pos + setting_len]) == setting_len:
+                setting = HTTP2Setting(s[pos: pos + setting_len])
+                # Populate our list of found settings
+                settings.append(setting)
+                # Move to the next settings
+                pos += setting_len
+            # Setting is too small too parse, pass it for further processing
+            else:
+                break
+        self.fields["settings"] = settings
+        return s[pos:]
+
+
+class HTTP2PushPromise(HTTP2PaddedFrame):
+    name = "HTTP2 %s Frame" % HTTP2_FRAME_TYPES[HTTP2FrameTypes.PUSH_PROMISE]
+    fields_desc = [ConditionalField(LenField("padding_length", None, fmt="B"), underlayer_has_padding_flag_set),
+                   IntField("promised_stream", 0),
+                   StrField("headers", "f\x87\x08\x9d\\\x0b\x81p\xff")]
+
+
+class HTTP2Ping(Packet):
+    name = "HTTP2 %s Frame" % HTTP2_FRAME_TYPES[HTTP2FrameTypes.PING]
+    fields_desc = [StrFixedLenField("data", "ping" * 2, 8)]
+
+
+class HTTP2GoAway(Packet):
+    name = "HTTP2 %s Frame" % HTTP2_FRAME_TYPES[HTTP2FrameTypes.GOAWAY]
+    fields_desc = [IntField("last_stream", 0),
+                   IntEnumField("error_code", HTTP2ErrorCodes.NO_ERROR, HTTP2_ERROR_CODES),
+                   StrField("debug_data", "")]
+
+
+class HTTP2WindowUpdate(Packet):
+    name = "HTTP2 %s Frame" % HTTP2_FRAME_TYPES[HTTP2FrameTypes.WINDOW_UPDATE]
+    fields_desc = [IntField("window_size", 65535)]
+
+
+class HTTP2Continuation(Packet):
+    name = "HTTP2 %s Frame" % HTTP2_FRAME_TYPES[HTTP2FrameTypes.CONTINUATION]
+    fields_desc = [StrField("headers", "f\x87\x08\x9d\\\x0b\x81p\xff")]
+
 
 class HTTP2(Packet):
     name = "HTTP2"
@@ -157,18 +222,13 @@ class HTTP2(Packet):
         frame_header_len = len(frame())
 
         frames = []
-        # Consume all bytes passed to us by the underlayer. We're expecting no
-        # further payload on top of us. If there is additional data on top of our layer
-        # We will incorrectly parse it
         while pos < len(raw_bytes) - frame_header_len:
             payload_len = frame(raw_bytes[pos: pos + frame_header_len]).length
             payload = frame(raw_bytes[pos: pos + frame_header_len + payload_len])
             # Populate our list of found frames
             frames.append(payload)
-            # Move to the next record
             pos += (frame_header_len + payload.length)
         self.fields["frames"] = frames
-        # This will always be empty (equivalent to returning "")
         return raw_bytes[pos:]
 
 
@@ -178,20 +238,17 @@ def pack_headers(encoder, headers):
 def unpack_headers(decoder, str_):
     return decoder.decode(str_)
 
-def set_msb(str_):
-    first_byte = ord(str_[0])
-    return "%s%s" % (chr(first_byte | 0x80), str_[1:])
+def set_msb(stream_id):
+    return stream_id | 0x80000000
 
-def unset_msb(str_):
-    first_byte = ord(str_[0])
-    return "%s%s" % (chr(first_byte & ~0x80), str_[1:])
+def unset_msb(stream_id):
+    return stream_id & ~0x80000000
 
-def toggle_msb(str_):
-    first_byte = ord(str_[0])
-    return "%s%s" % (chr(first_byte ^ 0x80), str_[1:])
+def toggle_msb(stream_id):
+    return stream_id ^ 0x80000000
 
 def generate_stream_id():
-    return unset_msb(os.urandom(32))
+    return unset_msb(random.randint(0, 2**32 -1))
 
 set_dependency_e_flag = set_msb
 unset_dependency_e_flag = unset_msb
@@ -203,3 +260,9 @@ bind_layers(HTTP2Frame, HTTP2Data, type=HTTP2FrameTypes.DATA)
 bind_layers(HTTP2Frame, HTTP2Headers, type=HTTP2FrameTypes.HEADERS)
 bind_layers(HTTP2Frame, HTTP2Priority, type=HTTP2FrameTypes.PRIORITY)
 bind_layers(HTTP2Frame, HTTP2RstStream, type=HTTP2FrameTypes.RST_STREAM)
+bind_layers(HTTP2Frame, HTTP2Settings, type=HTTP2FrameTypes.SETTINGS)
+bind_layers(HTTP2Frame, HTTP2PushPromise, type=HTTP2FrameTypes.PUSH_PROMISE)
+bind_layers(HTTP2Frame, HTTP2Ping, type=HTTP2FrameTypes.PING)
+bind_layers(HTTP2Frame, HTTP2GoAway, type=HTTP2FrameTypes.GOAWAY)
+bind_layers(HTTP2Frame, HTTP2WindowUpdate, type=HTTP2FrameTypes.WINDOW_UPDATE)
+bind_layers(HTTP2Frame, HTTP2Continuation, type=HTTP2FrameTypes.CONTINUATION)
--- tests/test_http2.py
+++ tests/test_http2.py
@@ -2,110 +2,139 @@
 
 import unittest
 
+from scapy.packet import Raw
 from http2 import *
 
 
 class TestHTTP2FrameHeader(unittest.TestCase):
-
     def test_built_and_dissected_packet_are_identical(self):
         payload = "A" * 10
-        id_ = os.urandom(32)
-        built_pkt = HTTP2Frame(type=HTTP2FrameTypes.HEADERS, flags=HTTP2Flags.PRIORITY, stream_id=id_)/payload
+        id_ = 0xabcd
+        built_pkt = HTTP2Frame(type=HTTP2FrameTypes.HEADERS, flags=HTTP2Flags.PRIORITY, stream=id_) / payload
         pkt = HTTP2Frame(str(built_pkt))
         self.assertEqual(HTTP2FrameTypes.HEADERS, pkt.type)
         self.assertEqual(HTTP2Flags.PRIORITY, pkt.flags)
-        self.assertEqual(id_, pkt.stream_id)
+        self.assertEqual(id_, pkt.stream)
         self.assertEqual(payload, str(pkt.payload))
         self.assertEqual(len(payload), pkt.length)
 
 
 class TestHTTP2PaddedFrame(unittest.TestCase):
-
     def test_when_padding_flag_is_unset_padding_is_disabled(self):
         data = "A" * 10
-        built_pkt = HTTP2Frame(flags=HTTP2Flags.END_STREAM)/HTTP2Data(data=data)
+        built_pkt = HTTP2Frame(flags=HTTP2Flags.ACK) / HTTP2Data(data=data)
         pkt = HTTP2Frame(str(built_pkt))
         self.assertIsNone(pkt.padding_length)
-        self.assertEqual("", pkt.padding)
+        self.assertFalse(pkt.haslayer(Padding))
 
     def test_when_padding_flag_is_set_padding_is_enabled(self):
         data = "A" * 10
         padding = "B" * 5
-        built_pkt = HTTP2Frame(flags=HTTP2Flags.PADDED)/HTTP2Data(data=data, padding=padding)
+        built_pkt = HTTP2Frame(flags=HTTP2Flags.PADDED) / HTTP2Data(data=data) / Padding(load=padding)
         pkt = HTTP2Frame(str(built_pkt))
         self.assertTrue(pkt.haslayer(HTTP2Data))
         self.assertEqual(len(padding), pkt[HTTP2Data].padding_length)
-        self.assertEqual(padding, pkt[HTTP2Data].padding)
+        self.assertEqual(padding, pkt[Padding].load)
 
 
 class TestHTTP2Headers(unittest.TestCase):
-
     def test_when_priority_flag_is_set_stream_dependency_and_weight_are_enabled(self):
-        stream_dependency = "D" * 32
+        dependency_stream = 0x9876
         weight = 0xff
-        built_pkt = HTTP2Frame(flags=HTTP2Flags.PRIORITY)/HTTP2Headers(dependency=stream_dependency, weight=weight)
+        built_pkt = HTTP2Frame(flags=HTTP2Flags.PRIORITY) / HTTP2Headers(dependency_stream=dependency_stream,
+                                                                         weight=weight)
         pkt = HTTP2Frame(str(built_pkt))
         self.assertTrue(pkt.haslayer(HTTP2Headers))
-        self.assertEqual(stream_dependency, pkt[HTTP2Headers].dependency)
+        self.assertEqual(dependency_stream, pkt[HTTP2Headers].dependency_stream)
         self.assertEqual(weight, pkt[HTTP2Headers].weight)
 
     def test_when_priority_flag_is_unset_stream_dependency_and_weight_are_disabled(self):
         headers = "X-Some-Header: some-value"
-        built_pkt = HTTP2Frame(flags=HTTP2Flags.UNUSED_2)/HTTP2Headers(headers=headers)
+        built_pkt = HTTP2Frame(flags=HTTP2Flags.UNUSED_2) / HTTP2Headers(headers=headers)
         pkt = HTTP2Frame(str(built_pkt))
         self.assertTrue(pkt.haslayer(HTTP2Headers))
-        self.assertIsNone(pkt[HTTP2Headers].dependency)
+        self.assertIsNone(pkt[HTTP2Headers].dependency_stream)
         self.assertIsNone(pkt[HTTP2Headers].weight)
 
     def test_when_both_priority_and_padded_flag_are_set_all_options_are_enabled(self):
-        stream_dependency = "D" * 32
+        stream_dependency = 0x1234
         weight = 0xff
         padding = "P" * 23
-        built_pkt = HTTP2Frame(flags=HTTP2Flags.PADDED | HTTP2Flags.PRIORITY)/HTTP2Headers(dependency=stream_dependency, weight=weight, padding=padding)
+        built_pkt = HTTP2Frame(flags=HTTP2Flags.PADDED | HTTP2Flags.PRIORITY) / HTTP2Headers(
+            dependency_stream=stream_dependency, weight=weight) / Padding(load=padding)
         pkt = HTTP2Frame(str(built_pkt))
         self.assertTrue(pkt.haslayer(HTTP2Headers))
-        self.assertEqual(stream_dependency, pkt[HTTP2Headers].dependency)
+        self.assertEqual(stream_dependency, pkt[HTTP2Headers].dependency_stream)
         self.assertEqual(weight, pkt[HTTP2Headers].weight)
-        self.assertEqual(padding, pkt[HTTP2Headers].padding)
+        self.assertEqual(padding, pkt[Padding].load)
 
     def test_when_both_priority_and_padded_flag_are_unset_all_options_are_disabled(self):
         headers = "X-Some-Header: some-value"
-        built_pkt = HTTP2Frame(flags=HTTP2Flags.END_HEADERS)/HTTP2Headers(headers=headers)
+        built_pkt = HTTP2Frame(flags=HTTP2Flags.END_HEADERS) / HTTP2Headers(headers=headers)
         pkt = HTTP2Frame(str(built_pkt))
         self.assertTrue(pkt.haslayer(HTTP2Headers))
         self.assertIsNone(pkt[HTTP2Headers].padding_length)
-        self.assertIsNone(pkt[HTTP2Headers].dependency)
+        self.assertIsNone(pkt[HTTP2Headers].dependency_stream)
         self.assertIsNone(pkt[HTTP2Headers].weight)
-        self.assertEqual("", pkt[HTTP2Headers].padding)
+        self.assertFalse(pkt.haslayer(Padding))
+
+
+class TestHTTP2Settings(unittest.TestCase):
+    def test_frame_containing_multiple_settings_is_correclty_dissected(self):
+        setting1 = HTTP2Setting(id=HTTP2SettingIds.SETTINGS_MAX_HEADER_LIST_SIZE, value=128)
+        setting2 = HTTP2Setting(id=HTTP2SettingIds.SETTINGS_MAX_CONCURRENT_STREAMS, value=100)
+        frame1 = HTTP2Frame() / HTTP2Settings(settings=[setting1, setting2]) / ("A" * 5)
+        frame2 = HTTP2Frame() / HTTP2RstStream(error_code="SETTINGS_TIMEOUT")
+        built_pkt = HTTP2.from_frames((frame1, frame2))
+        pkt = HTTP2(str(built_pkt))
+        self.assertTrue(pkt.frames[0].haslayer(HTTP2Settings))
+        self.assertEqual(2, len(pkt.frames[0][HTTP2Settings].settings))
+        self.assertEqual(setting1, pkt.frames[0][HTTP2Settings].settings[0])
+        self.assertEqual(setting2, pkt.frames[0][HTTP2Settings].settings[1])
+        self.assertEqual("A" * 5, str(pkt.frames[0][Raw]))
+        self.assertTrue(pkt.frames[1].haslayer(HTTP2RstStream))
+        self.assertEqual(str(frame2), str(pkt.frames[1]))
 
 
 class TestHTTP2(unittest.TestCase):
-
     def test_stacked_frame_packet_are_dissected_correctly(self):
-        frame1 = HTTP2Frame(stream_id="B" * 32, flags=HTTP2Flags.UNUSED_7)/HTTP2Headers()
-        frame2 = HTTP2Frame(flags=HTTP2Flags.PADDED | HTTP2Flags.PRIORITY, type=0xff)/HTTP2Data(padding="P" * 23)
-        frame3 = HTTP2Frame()/HTTP2Priority(dependency="D" * 32, weight=5)
-        built_pkt = HTTP2.from_frames([frame1, frame2, frame3])
+        frame1 = HTTP2Frame(stream=0x4, flags=HTTP2Flags.UNUSED_7) / HTTP2Headers()
+        frame2 = HTTP2Frame(flags=HTTP2Flags.PADDED | HTTP2Flags.PRIORITY) / HTTP2Data() / Padding(load="P" * 23)
+        frame3 = HTTP2Frame() / HTTP2Priority(dependency_stream=0x5, weight=5)
+        frame4 = HTTP2Frame() / HTTP2RstStream(error_code=HTTP2ErrorCodes.REFUSED_STREAM)
+        frame5 = HTTP2Frame(flags=HTTP2Flags.PADDED) / HTTP2PushPromise(promised_stream=0xffe34) / Padding(load="Z" * 9)
+        frame6 = HTTP2Frame(flags=HTTP2Flags.UNUSED_2 | HTTP2Flags.ACK) / HTTP2Ping()
+        frame7 = HTTP2Frame() / HTTP2GoAway(last_stream=0x567, error_code=HTTP2ErrorCodes.COMPRESSION_ERROR,
+                                            debug_data="jkl" * 43)
+        frame8 = HTTP2Frame() / HTTP2WindowUpdate(window_size=2 ** 31 - 1) / "Trailing Data"
+        frame9 = HTTP2Frame(flags=HTTP2Flags.PADDED) / HTTP2Continuation(headers="some-headers")
+        built_pkt = HTTP2.from_frames([frame1, frame2, frame3, frame4, frame5, frame6, frame7, frame8, frame9])
         pkt = HTTP2(str(built_pkt))
-        pkt.show()
-        self.assertTrue(len(pkt.frames) == 3)
+        self.assertTrue(len(pkt.frames) == 9)
         self.assertEqual(str(frame1), str(pkt.frames[0]))
         self.assertEqual(str(frame2), str(pkt.frames[1]))
         self.assertEqual(str(frame3), str(pkt.frames[2]))
+        self.assertEqual(str(frame4), str(pkt.frames[3]))
+        self.assertEqual(str(frame5), str(pkt.frames[4]))
+        self.assertTrue(pkt.frames[4].haslayer(Padding))
+        self.assertEqual(str(frame6), str(pkt.frames[5]))
+        self.assertEqual(str(frame7), str(pkt.frames[6]))
+        self.assertEqual(str(frame8), str(pkt.frames[7]))
+        self.assertTrue(pkt.frames[7].haslayer(Raw))
+        self.assertEqual(str(frame9), str(pkt.frames[8]))
 
 
 class TestTopLevelFunctions(unittest.TestCase):
-
-    def test_stream_id_first_bit_is_clear(self):
+    def test_when_generated_stream_id_first_bit_is_clear(self):
         for i in range(0, 0xff):
-            self.assertTrue(ord(generate_stream_id()[0]) & (1 << 7) == 0)
+            self.assertTrue(generate_stream_id() & (1 << 31) == 0)
 
     def test_when_flags_are_set_they_are_caught(self):
         pkt = HTTP2Frame(flags=HTTP2Flags.PRIORITY)
         self.assertTrue(has_flag_set(pkt, HTTP2Flags.PRIORITY))
         self.assertFalse(has_flag_set(pkt, HTTP2Flags.UNUSED_5))
-        pkt = HTTP2Frame(flags=HTTP2Flags.END_STREAM | HTTP2Flags.END_HEADERS | HTTP2Flags.UNUSED_8)
-        self.assertTrue(has_flag_set(pkt, HTTP2Flags.END_STREAM))
+        pkt = HTTP2Frame(flags=HTTP2Flags.ACK | HTTP2Flags.END_HEADERS | HTTP2Flags.UNUSED_8)
+        self.assertTrue(has_flag_set(pkt, HTTP2Flags.ACK))
         self.assertTrue(has_flag_set(pkt, HTTP2Flags.END_HEADERS))
         self.assertTrue(has_flag_set(pkt, HTTP2Flags.UNUSED_8))
         self.assertFalse(has_flag_set(pkt, HTTP2Flags.UNUSED_2))
@@ -113,5 +142,5 @@ class TestTopLevelFunctions(unittest.TestCase):
         self.assertFalse(has_flag_set(pkt, HTTP2Flags.UNUSED_7))
 
     def test_when_e_flag_is_set_first_bit_is_set(self):
-        stream_dependency_id = set_dependency_e_flag("%s%s" % (chr(0x7f), "S" * 31))
-        self.assertEqual(0x7f | 0x80, ord(stream_dependency_id[0]))
+        stream_dependency_id = set_dependency_e_flag(0x7fffffff)
+        self.assertTrue(stream_dependency_id & (1 << 31) >> 31 == 1)
